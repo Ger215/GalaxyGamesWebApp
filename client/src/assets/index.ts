b249746090541379693307ff9a3c47b63afc07a6
Ger215 <gsrp0310@gmail.com>
2024-08-04 22:12:03 -0300

    [FEAT] Search Bar

---
+++ client/src/assets/index.ts
@@ -0,0 +1,5 @@
+import logo from './logo.webp';
+import payment from './payment.webp';
+import productPayment from './productPayment.webp';
+
+export { logo, payment, productPayment };
--- client/src/ui/Header.tsx
+++ client/src/ui/Header.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { logo } from '../assets';
+import { IoClose, IoSearchOutline } from 'react-icons/io5';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="w-full bg-whiteText">
+      <div className="max-w-screen-xl mx-auto h-20 flex items-center justify-between lg:px-0">
+        <img src={logo} alt="logo" className="w-60" />
+        <div className="hidden md:inline-flex max-w-3xl w-full relative">
+          <input
+            type="text"
+            placeholder="Buscar"
+            className="w-full flex-1 rounded-full text-gray-900 text-lg placeholder:text-base placeholder:tracking-wide shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-300 placeholder:font-normal focus:ring-1 focus:ring-darkText sm:text-sm px-4 py-2"
+          />
+          <IoSearchOutline className="absolute top-2 right-4 text-xl" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
