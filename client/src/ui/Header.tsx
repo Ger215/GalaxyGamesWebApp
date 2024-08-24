11c4bf5c852f69835277dd93eb749c1ceffc5b9d
Ger215 <gsrp0310@gmail.com>
2024-08-24 14:53:32 -0300

    [FEAT] Search Bar value deletion

--- client/src/ui/Header.tsx
+++ client/src/ui/Header.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { logo } from '../assets';
 import { IoClose, IoSearchOutline } from 'react-icons/io5';
 
 const Header = () => {
+  const [searchText, setSearchText] = useState('');
   return (
     <div className="w-full bg-whiteText">
       <div className="max-w-screen-xl mx-auto h-20 flex items-center justify-between lg:px-0">
@@ -10,10 +11,19 @@ const Header = () => {
         <div className="hidden md:inline-flex max-w-3xl w-full relative">
           <input
             type="text"
+            onChange={(e) => setSearchText(e.target.value)}
+            value={searchText}
             placeholder="Buscar"
             className="w-full flex-1 rounded-full text-gray-900 text-lg placeholder:text-base placeholder:tracking-wide shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-300 placeholder:font-normal focus:ring-1 focus:ring-darkText sm:text-sm px-4 py-2"
           />
-          <IoSearchOutline className="absolute top-2 right-4 text-xl" />
+          {searchText ? (
+            <IoClose
+              className="absolute top-2 right-4 text-xl cursor-pointer hover:text-red-500 duration-200"
+              onClick={() => setSearchText('')}
+            />
+          ) : (
+            <IoSearchOutline className="absolute top-2 right-4 text-xl" />
+          )}
         </div>
       </div>
     </div>
