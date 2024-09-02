56191c26f2eeb47f4cdb66e1464c39877d5ca9af
Ger215 <gsrp0310@gmail.com>
2024-09-02 17:58:46 -0300

    Routing Setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,12 @@
-import Footer from './ui/Footer';
-import Header from './ui/Header';
 import Container from './ui/Container';
 import './App.css';
 
 function App() {
   return (
     <main>
-      <Header />
       <Container>
         <p>Ecommerce</p>
       </Container>
-      <Footer />
     </main>
   );
 }
--- client/src/ui/Header.tsx
+++ client/src/ui/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { logo } from '../assets';
 import { IoClose, IoSearchOutline } from 'react-icons/io5';
 import { FiShoppingCart, FiUser } from 'react-icons/fi';
